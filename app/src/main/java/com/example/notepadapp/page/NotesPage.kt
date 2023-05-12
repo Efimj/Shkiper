c1a956aaa37b91a078584ed47ad51e90323483e5
EFFEMole <go.netgle@gmail.com>
2023-05-12 18:54:11 +0300

    Автоматически скрывающееся поле

--- app/src/main/java/com/example/notepadapp/page/NotesPage.kt
+++ app/src/main/java/com/example/notepadapp/page/NotesPage.kt
@@ -1,18 +1,29 @@
 package com.example.notepadapp.page
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
-import com.example.notepadapp.ui.components.cards.NoteCard
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.notepadapp.ui.components.fields.SearchField
+import com.example.notepadapp.ui.components.layouts.NotesVerticalMasonry
 
 data class Card(
     var title: String,
@@ -51,40 +62,82 @@ fun NotesPage() {
         Card("Список цитат", "перечень любимых цитат, которые вдохновляют и мотивируют на достижение целей."),
     )
     val (selectedCardIndices, setSelectedCardIndices) = remember { mutableStateOf(setOf<Int>()) }
+
+    val viewModel = ExampleViewModel()
+    val scrollState = rememberLazyStaggeredGridState()
+    val scrollUpState = viewModel.scrollUp.observeAsState()
+
+    viewModel.updateScrollPosition(scrollState.firstVisibleItemIndex)
+
+    Box(Modifier.fillMaxSize().padding(horizontal = 10.dp)) {
+        NotesVerticalMasonry(
+            staggeredGridCellsMode,
+            noteCards,
+            selectedCardIndices,
+            scrollState,
+            PaddingValues(0.dp, 70.dp, 0.dp, 100.dp),
+            { onNoteClick(selectedCardIndices, it, setSelectedCardIndices) }
+        ) { changeSelectedMode(selectedCardIndices, it, setSelectedCardIndices) }
+        ScrollableAppBar(
+            scrollUpState = scrollUpState
+        )
+//        Box(
+//            Modifier.padding(horizontal = 10.dp).background(Color.Transparent).align(Alignment.CenterStart)
+//        ) {
+//            SearchField(search = search, onValueChange = {
+//                search = it
+//            })
+//        }
+    }
+}
+
+@Composable
+fun ScrollableAppBar(
+    scrollUpState: State<Boolean?>,
+) {
+    val position by animateFloatAsState(if (scrollUpState.value == true) -170f else 0f)
     var search by remember { mutableStateOf("Value") }
 
-
-    Box(Modifier.fillMaxSize()) {
-        Column(Modifier.padding(horizontal = 10.dp)) {
-            Box(Modifier.padding(horizontal = 10.dp).background(Color.Transparent)) {
-                SearchField(search = search, onValueChange = {
-                    search = it
-                })
-            }
-            LazyVerticalStaggeredGrid(
-                columns = staggeredGridCellsMode,
-                verticalItemSpacing = 10.dp,
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                contentPadding = PaddingValues(0.dp, 10.dp, 0.dp, 100.dp),
-                content = {
-                    items(noteCards.size) { index ->
-                        NoteCard(
-                            noteCards[index].title,
-                            noteCards[index].description,
-                            selected = index in selectedCardIndices,
-                            onClick = {
-                                if (selectedCardIndices.isNotEmpty())
-                                    changeSelectedMode(selectedCardIndices, index, setSelectedCardIndices)
-                            },
-                            onLongClick = {
-                                changeSelectedMode(selectedCardIndices, index, setSelectedCardIndices)
-                            })
-                    }
-                },
-                modifier = Modifier.fillMaxSize()
-            )
-        }
+    Box(
+        Modifier
+            .padding(top = 10.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(Color.Transparent)
+            .graphicsLayer { translationY = (position) }
+            .clip(RoundedCornerShape(15.dp))
+            .padding(horizontal = 10.dp)
+            .background(Color.Transparent),
+    ) {
+        SearchField(search = search, onValueChange = {
+            search = it
+        })
     }
+
+}
+
+class ExampleViewModel : ViewModel() {
+
+    private var lastScrollIndex = 0
+
+    private val _scrollUp = MutableLiveData(false)
+    val scrollUp: LiveData<Boolean>
+        get() = _scrollUp
+
+    fun updateScrollPosition(newScrollIndex: Int) {
+        if (newScrollIndex == lastScrollIndex) return
+
+        _scrollUp.value = newScrollIndex > lastScrollIndex
+        lastScrollIndex = newScrollIndex
+    }
+}
+
+private fun onNoteClick(
+    selectedCardIndices: Set<Int>,
+    index: Int,
+    setSelectedCardIndices: (Set<Int>) -> Unit
+) {
+    if (selectedCardIndices.isNotEmpty())
+        changeSelectedMode(selectedCardIndices, index, setSelectedCardIndices)
 }
 
 private fun changeSelectedMode(
