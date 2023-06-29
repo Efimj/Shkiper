a815c491f05fe8b9d0cc2a2f2f3ee41d94e88c09
EFFEMole <go.netgle@gmail.com>
2023-06-29 11:40:33 +0300

    update

--- app/src/main/java/com/example/notepadapp/activity/SplashActivity.kt
+++ app/src/main/java/com/example/notepadapp/activity/SplashActivity.kt
@@ -4,9 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.notepadapp.app_handlers.ThemePreferenceManager
+import com.example.notepadapp.screens.NoteListScreen.NotesViewModel
+import com.example.notepadapp.screens.NoteScreen.NoteViewModel
 import com.example.notepadapp.util.ThemeUtil
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,13 +24,18 @@ import kotlinx.coroutines.launch
 @ExperimentalPagerApi
 @AndroidEntryPoint
 @OptIn(DelicateCoroutinesApi::class)
-class SplashActivity  : ComponentActivity() {
+class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
 
         splashScreen.setKeepOnScreenCondition { true }
+
+        // Initialization Realm database
+        setContent {
+            val noteViewModel: NotesViewModel = hiltViewModel()
+        }
 
         GlobalScope.launch {
             delay(300)
