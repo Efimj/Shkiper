c2e1573467b7ff5ab01ab3a44ccf541f2c1e8b5b
EFFemole <go.netgle@gmail.com>
2023-07-13 21:23:58 +0300

    update

--- app/src/main/java/com/android/notepad/screens/StatisticsScreen/StatisticsScreen.kt
+++ app/src/main/java/com/android/notepad/screens/StatisticsScreen/StatisticsScreen.kt
@@ -1,13 +1,11 @@
 package com.android.notepad.screens.StatisticsScreen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -15,8 +13,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android.notepad.R
 import com.android.notepad.services.statistics_service.StatisticsStorage
+import com.android.notepad.ui.theme.CustomAppTheme
 
 @Composable
 fun StatisticsScreen() {
@@ -30,9 +33,13 @@ fun StatisticsScreen() {
             item(span = {
                 GridItemSpan(maxLineSpan)
             }) {
-                Row {
-                    Text("Statistics", color = Color.White)
-                    Text(statistics.openAppCount.toString(), color = Color.White)
+                Row (modifier = Modifier.padding(top = 45.dp, bottom = 10.dp),horizontalArrangement = Arrangement.Center){
+                    Text(
+                        stringResource(R.string.Statistics),
+                        color = CustomAppTheme.colors.text,
+                        style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold),
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
                 }
             }
             for (s in 0..33)
