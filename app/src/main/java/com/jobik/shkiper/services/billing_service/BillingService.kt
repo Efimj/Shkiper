f6108289831f52529cb136250a4e5c828ef8fa5f
EFFemole <go.netgle@gmail.com>
2023-08-10 20:54:33 +0300

    updated billing client

--- app/src/main/java/com/jobik/shkiper/services/billing_service/BillingService.kt
+++ app/src/main/java/com/jobik/shkiper/services/billing_service/BillingService.kt
@@ -1,16 +1,21 @@
 package com.jobik.shkiper.services.billing_service
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import com.android.billingclient.api.*
+import kotlinx.coroutines.*
+import java.lang.Runnable
 import kotlin.math.pow
 
 class BillingService private constructor(
-    private val context: Context
+    private val context: Context,
+    private val externalScope: CoroutineScope =
+        CoroutineScope(SupervisorJob() + Dispatchers.Default)
 ) : PurchasesUpdatedListener, BillingClientStateListener {
     private var billingClient = BillingClient.newBuilder(context)
         .setListener(this)
@@ -106,6 +111,87 @@ class BillingService private constructor(
             // process returned productDetailsList
             if (billingResult.responseCode == BillingClient.BillingResponseCode.OK)
                 basicProductWithProductDetails.value = productDetailsList
+        }
+    }
+
+    fun makePurchase(activity: Activity, productDetails: ProductDetails) {
+        val productDetailsParamsList =
+            if (productDetails.productType == BillingClient.ProductType.INAPP)
+                listOf(
+                    BillingFlowParams.ProductDetailsParams.newBuilder()
+                        .setProductDetails(productDetails)
+                        .build()
+                )
+            else
+                listOf(
+                    productDetails.subscriptionOfferDetails?.get(0)?.let {
+                        BillingFlowParams.ProductDetailsParams.newBuilder()
+                            .setProductDetails(productDetails)
+                            // to get an offer token, call ProductDetails.subscriptionOfferDetails()
+                            // for a list of offers that are available to the user
+                            .setOfferToken(it.offerToken)
+                            .build()
+                    }
+                )
+
+        val billingFlowParams = BillingFlowParams.newBuilder()
+            .setProductDetailsParamsList(productDetailsParamsList)
+            .build()
+
+        val billingResult = billingClient.launchBillingFlow(activity, billingFlowParams)
+    }
+
+    /**
+     * Called by the Billing Library when new purchases are detected.
+     */
+    override fun onPurchasesUpdated(
+        billingResult: BillingResult,
+        purchases: MutableList<Purchase>?
+    ) {
+        val responseCode = billingResult.responseCode
+        val debugMessage = billingResult.debugMessage
+        Log.d(TAG, "onPurchasesUpdated: $responseCode $debugMessage")
+        when (responseCode) {
+            BillingClient.BillingResponseCode.OK -> {
+                // ToDo Make response
+                if (purchases != null) {
+                    externalScope.launch {
+                        handlePurchase(purchases)
+                    }
+                }
+            }
+
+            BillingClient.BillingResponseCode.USER_CANCELED -> {
+                Log.i(TAG, "onPurchasesUpdated: User canceled the purchase")
+            }
+
+            BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED -> {
+                Log.i(TAG, "onPurchasesUpdated: The user already owns this item")
+            }
+
+            BillingClient.BillingResponseCode.DEVELOPER_ERROR -> {
+                Log.e(
+                    TAG, "onPurchasesUpdated: Developer error means that Google Play does " +
+                            "not recognize the configuration. If you are just getting started, " +
+                            "make sure you have configured the application correctly in the " +
+                            "Google Play Console. The product ID must match and the APK you " +
+                            "are using must be signed with release keys."
+                )
+            }
+        }
+    }
+
+    private suspend fun handlePurchase(purchases: MutableList<Purchase>) {
+        purchases.forEach { purchase ->
+            if (purchase.purchaseState === Purchase.PurchaseState.PURCHASED) {
+                if (!purchase.isAcknowledged) {
+                    val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+                        .setPurchaseToken(purchase.purchaseToken)
+                    val ackPurchaseResult = withContext(Dispatchers.IO) {
+                        billingClient.acknowledgePurchase(acknowledgePurchaseParams.build())
+                    }
+                }
+            }
         }
     }
 
