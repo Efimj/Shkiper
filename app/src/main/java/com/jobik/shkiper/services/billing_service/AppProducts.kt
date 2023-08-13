075a8797ec8a415b44e8599193854342f65db577
EFFemole <go.netgle@gmail.com>
2023-08-13 12:09:19 +0300

    updated billing client

--- app/src/main/java/com/jobik/shkiper/services/billing_service/AppProducts.kt
+++ app/src/main/java/com/jobik/shkiper/services/billing_service/AppProducts.kt
@@ -7,10 +7,13 @@ object AppProducts {
     //Product IDs
     const val ExampleProduct = "cup_of_tea"
 
+    //Subscriptions IDs
+    const val sProduct = "cup_of_tea"
+
     val ListOfProducts = listOf(
         ExampleProduct
     )
     val ListOfSubscriptions = listOf(
-        ExampleProduct
+        sProduct
     )
 }
