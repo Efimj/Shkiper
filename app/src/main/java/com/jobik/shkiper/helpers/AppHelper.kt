65fac92a19f4b58bf92321d98d7987b92d70afa3
EFFemole <go.netgle@gmail.com>
2023-11-04 15:52:48 +0200

    update

--- app/src/main/java/com/jobik/shkiper/helpers/AppHelper.kt
+++ app/src/main/java/com/jobik/shkiper/helpers/AppHelper.kt
@@ -5,9 +5,12 @@ import android.content.Context
 import androidx.core.app.NotificationManagerCompat
 
 fun areChanelNotificationsEnabled(context: Context, channelId: String): Boolean {
-    val notificationManager = context.getSystemService(NotificationManager::class.java)
-    val channel = notificationManager.getNotificationChannel(channelId)
-    return channel.importance != NotificationManager.IMPORTANCE_NONE
+    runCatching {
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        val channel = notificationManager.getNotificationChannel(channelId)
+        return channel.importance != NotificationManager.IMPORTANCE_NONE
+    }
+    return false
 }
 
 fun areNotificationsEnabled(context: Context): Boolean {
