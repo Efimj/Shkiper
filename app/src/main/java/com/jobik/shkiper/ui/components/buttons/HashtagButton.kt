e9347157dc3a083215bfd150962c4800e0c5cc7a
EFFemole <go.netgle@gmail.com>
2024-01-26 16:55:00 +0200

    updated: hashtag button

--- app/src/main/java/com/jobik/shkiper/ui/components/buttons/HashtagButton.kt
+++ app/src/main/java/com/jobik/shkiper/ui/components/buttons/HashtagButton.kt
@@ -1,5 +1,6 @@
 package com.jobik.shkiper.ui.components.buttons
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.basicMarquee
@@ -7,10 +8,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.jobik.shkiper.screens.OnboardingScreen.OnBoardingPage
 import com.jobik.shkiper.ui.helpers.MultipleEventsCutter
 import com.jobik.shkiper.ui.helpers.get
 import com.jobik.shkiper.ui.theme.CustomTheme
@@ -20,22 +24,35 @@ import com.jobik.shkiper.ui.theme.CustomTheme
 fun HashtagButton(chip: String, selected: Boolean, onChipClicked: (String) -> Unit) {
     val multipleEventsCutter = remember { MultipleEventsCutter.get() }
 
+    val buttonContentColor: Color by animateColorAsState(
+        targetValue = if (selected) CustomTheme.colors.textOnActive else CustomTheme.colors.text,
+        label = "buttonContentColor"
+    )
+
+    val buttonBackgroundColor: Color by animateColorAsState(
+        targetValue = if (selected) CustomTheme.colors.active else CustomTheme.colors.secondaryBackground,
+        label = "buttonBackgroundColor"
+    )
+
     Chip(
         modifier = Modifier
             .padding(end = 8.dp),
         onClick = { multipleEventsCutter.processEvent { onChipClicked(chip) } },
         shape = RoundedCornerShape(10.dp),
         colors = ChipDefaults.chipColors(
-            backgroundColor = if (selected) CustomTheme.colors.active else CustomTheme.colors.secondaryBackground,
-            contentColor = if (selected) CustomTheme.colors.textOnActive else CustomTheme.colors.text
+            backgroundColor = buttonBackgroundColor,
+            contentColor = buttonContentColor
         ),
     ) {
         Text(
             chip,
-            modifier = Modifier.basicMarquee().padding(8.dp),
+            modifier = Modifier
+                .basicMarquee()
+                .padding(8.dp),
             maxLines = 1,
             style = MaterialTheme.typography.body1,
-            color = if (selected) Color.White else CustomTheme.colors.text
+            color = buttonContentColor,
+            fontWeight = if (selected) FontWeight.SemiBold else FontWeight.Normal
         )
     }
 }
