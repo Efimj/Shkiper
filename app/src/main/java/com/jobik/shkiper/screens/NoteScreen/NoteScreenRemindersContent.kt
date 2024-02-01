e8cf64f9a0aaa01f94a68be4f8a508d554c7dbda
EFFemole <go.netgle@gmail.com>
2024-02-01 14:36:39 +0200

    updated string resources, reminder card

--- app/src/main/java/com/jobik/shkiper/screens/NoteScreen/NoteScreenRemindersContent.kt
+++ app/src/main/java/com/jobik/shkiper/screens/NoteScreen/NoteScreenRemindersContent.kt
@@ -1,20 +1,33 @@
 package com.jobik.shkiper.screens.NoteScreen
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.glance.appwidget.lazy.LazyColumn
-import androidx.glance.layout.Box
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.jobik.shkiper.R
+import com.jobik.shkiper.ui.components.cards.ReminderCard
 import com.jobik.shkiper.ui.components.modals.CustomModalBottomSheet
 import com.jobik.shkiper.ui.components.modals.ReminderDialogProperties
+import com.jobik.shkiper.ui.theme.CustomTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteScreenRemindersContent(noteViewModel: NoteViewModel) {
     val context = LocalContext.current
-    val shareSheetState = androidx.compose.material3.rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val shareSheetState = androidx.compose.material3.rememberModalBottomSheetState()
 
     LaunchedEffect(noteViewModel.screenState.value.isCreateReminderDialogShow) {
         if (!noteViewModel.screenState.value.isCreateReminderDialogShow) {
@@ -31,12 +44,62 @@ fun NoteScreenRemindersContent(noteViewModel: NoteViewModel) {
             dragHandle = null,
         ) {
             Box {
-                LazyColumn(){
+                LazyColumn(
+                    contentPadding = PaddingValues(horizontal = 20.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    item {
+                        noteViewModel.screenState.value.reminder.let {
+                            if (it != null)
+                                ReminderCard(reminder = it) {
 
+                                }
+                        }
+
+                    }
+                    item {
+                        Spacer(modifier = Modifier.padding(bottom = 80.dp))
+                    }
                 }
-
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(horizontal = 30.dp)
+                        .padding(bottom = 10.dp)
+                ) {
+                    BottomBar()
+                }
             }
         }
+    }
+}
+
+@Composable
+private fun BottomBar() {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        shape = CustomTheme.shapes.small,
+        colors = ButtonDefaults.buttonColors(
+            contentColor = CustomTheme.colors.textOnActive,
+            containerColor = CustomTheme.colors.active
+        ),
+        border = null,
+        elevation = null,
+        contentPadding = PaddingValues(horizontal = 15.dp),
+        onClick = {
+
+        }
+    ) {
+        Text(
+            text = stringResource(R.string.CreateReminder),
+            style = MaterialTheme.typography.body1,
+            fontWeight = FontWeight.SemiBold,
+            color = CustomTheme.colors.textOnActive,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
--- app/src/main/java/com/jobik/shkiper/ui/components/cards/ReminderCard.kt
+++ app/src/main/java/com/jobik/shkiper/ui/components/cards/ReminderCard.kt
@@ -1,6 +1,8 @@
 package com.jobik.shkiper.ui.components.cards
 
+import androidx.annotation.Keep
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -10,6 +12,7 @@ import androidx.compose.material.icons.outlined.Event
 import androidx.compose.material.icons.outlined.Repeat
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -26,6 +29,7 @@ import com.jobik.shkiper.database.models.RepeatMode
 import com.jobik.shkiper.helpers.DateHelper
 import com.jobik.shkiper.ui.modifiers.bounceClick
 import com.jobik.shkiper.ui.theme.CustomTheme
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -33,10 +37,26 @@ fun ReminderCard(reminder: Reminder, onClick: () -> Unit) {
     val context = LocalContext.current
     val nextReminderDate = DateHelper.nextDateWithRepeating(reminder.date, reminder.time, reminder.repeat)
     val isDateFuture = DateHelper.isFutureDateTime(nextReminderDate)
+    val isRepeatable = reminder.repeat != RepeatMode.NONE
 
     val backgroundColor by animateColorAsState(
         if (isDateFuture) CustomTheme.colors.secondaryBackground else CustomTheme.colors.mainBackground,
         label = "ReminderCardBackgroundColor",
+    )
+
+    val strokeColor by animateColorAsState(
+        if (isDateFuture) CustomTheme.colors.secondaryBackground else CustomTheme.colors.stroke,
+        label = "ReminderCardStrokeColor",
+    )
+
+    val contentColor by animateColorAsState(
+        if (isDateFuture && isDateFuture) CustomTheme.colors.text else CustomTheme.colors.textSecondary,
+        label = "ReminderCardStrokeColor",
+    )
+
+    val iconSize by animateDpAsState(
+        if (isRepeatable) 25.dp else 35.dp,
+        label = "iconSize",
     )
 
     Card(
@@ -47,28 +67,31 @@ fun ReminderCard(reminder: Reminder, onClick: () -> Unit) {
             disabledContainerColor = backgroundColor,
             disabledContentColor = CustomTheme.colors.text
         ),
-        border = BorderStroke(width = 1.dp, CustomTheme.colors.secondaryBackground),
+        border = BorderStroke(width = 2.dp, strokeColor),
         elevation = CardDefaults.outlinedCardElevation(),
         modifier = Modifier
+            .fillMaxWidth()
             .bounceClick(0.95f)
             .clip(CustomTheme.shapes.medium)
             .clickable { onClick() }
     ) {
         Row(
-            modifier = Modifier.padding(20.dp),
+            modifier = Modifier.padding(15.dp),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(20.dp)
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            Column(modifier = Modifier.heightIn(min = 40.dp)) {
-                Image(
-                    imageVector = if (reminder.repeat == RepeatMode.NONE) Icons.Outlined.Event else Icons.Outlined.Repeat,
-                    contentDescription = stringResource(
-                        id = R.string.Repeat
-                    ),
-                    contentScale = ContentScale.FillHeight,
-                    modifier = Modifier.fillMaxHeight()
+            Column(
+                modifier = Modifier.heightIn(min = 40.dp),
+                verticalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = if (isRepeatable) Icons.Outlined.Repeat else Icons.Outlined.Event,
+                    contentDescription = stringResource(id = R.string.Repeat),
+                    modifier = Modifier.size(iconSize),
+                    tint = contentColor
                 )
-                if (reminder.repeat != RepeatMode.NONE)
+                if (isRepeatable)
                     Text(
                         text = reminder.repeat.getLocalizedValue(context = context),
                         maxLines = 1,
@@ -77,15 +100,25 @@ fun ReminderCard(reminder: Reminder, onClick: () -> Unit) {
                         color = CustomTheme.colors.textSecondary,
                     )
             }
-            Text(
-                text = DateHelper.getLocalizedDate(nextReminderDate.toLocalDate()),
-                minLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.h6,
-                color = CustomTheme.colors.textSecondary,
+            Row(
                 modifier = Modifier.basicMarquee()
-            )
-
+            ) {
+                Text(
+                    text = DateHelper.getLocalizedDate(nextReminderDate.toLocalDate()),
+                    minLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.h6,
+                    color = contentColor,
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = nextReminderDate.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")),
+                    minLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.h6,
+                    color = contentColor,
+                )
+            }
         }
     }
 }
