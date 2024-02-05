a326ca480f2b01ddeaba5ed91e10cf8874c47bed
EFFemole <go.netgle@gmail.com>
2024-02-05 13:39:48 +0200

    refactored

---
+++ app/src/main/java/com/jobik/shkiper/screens/NoteListScreen/NoteListScreenContent.kt
@@ -0,0 +1,296 @@
+package com.jobik.shkiper.screens.NoteListScreen
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.*
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Description
+import androidx.compose.material.icons.outlined.Event
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.jobik.shkiper.R
+import com.jobik.shkiper.navigation.AppScreens
+import com.jobik.shkiper.ui.components.buttons.FloatingActionButton
+import com.jobik.shkiper.ui.components.buttons.HashtagButton
+import com.jobik.shkiper.ui.components.cards.NoteCard
+import com.jobik.shkiper.ui.components.fields.SearchBar
+import com.jobik.shkiper.ui.components.fields.SearchBarActionButton
+import com.jobik.shkiper.ui.components.layouts.BannerList
+import com.jobik.shkiper.ui.components.layouts.LazyGridNotes
+import com.jobik.shkiper.ui.components.layouts.ScreenContentIfNoData
+import com.jobik.shkiper.ui.components.modals.CreateReminderDialog
+import com.jobik.shkiper.ui.components.modals.ReminderDialogProperties
+import com.jobik.shkiper.ui.theme.CustomTheme
+import com.jobik.shkiper.viewmodels.NotesViewModel
+
+@Composable
+fun NoteListScreenContent(
+    navController: NavController,
+    notesViewModel: NotesViewModel
+) {
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route ?: ""
+
+    val searchBarHeight = 60.dp
+    val actionBarHeight = 56.dp
+
+    val searchBarOffsetHeightPx = remember { mutableFloatStateOf(0f) }
+    val searchBarHeightPx = with(LocalDensity.current) { searchBarHeight.roundToPx().toFloat() }
+    val lazyGridNotes = rememberLazyStaggeredGridState()
+
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                val delta = available.y
+                val newOffset = searchBarOffsetHeightPx.floatValue + delta
+                if (lazyGridNotes.canScrollForward) searchBarOffsetHeightPx.floatValue =
+                    newOffset.coerceIn(-searchBarHeightPx, 0f)
+                return Offset.Zero
+            }
+        }
+    }
+
+    val actionBarHeightPx = with(LocalDensity.current) { actionBarHeight.roundToPx().toFloat() }
+    val offsetX = remember { Animatable(-actionBarHeightPx) }
+
+    BackHandlerIfSelectedNotes(notesViewModel)
+    IfSelectedNotesChanged(notesViewModel, offsetX, actionBarHeightPx)
+    IfScrollingImposible(lazyGridNotes, searchBarOffsetHeightPx)
+    WhenUserCreateNewNote(notesViewModel, navController)
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .nestedScroll(nestedScrollConnection)
+    ) {
+        if (notesViewModel.screenState.value.isNotesInitialized && notesViewModel.screenState.value.notes.isEmpty())
+            ScreenContentIfNoData(title = R.string.EmptyNotesPageHeader, icon = Icons.Outlined.Description)
+        else
+            NotesListContent(notesViewModel, lazyGridNotes, navController, currentRoute)
+        Box(modifier = Modifier) {
+            SearchBar(
+                searchBarContainerHeight = searchBarHeight,
+                searchBarOffsetHeightPx = searchBarOffsetHeightPx.floatValue,
+                isVisible = notesViewModel.screenState.value.selectedNotes.isEmpty(),
+                value = notesViewModel.screenState.value.searchText,
+                actionButton = SearchBarActionButton(
+                    icon = Icons.Outlined.Event,
+                    contentDescription = R.string.Reminders,
+                    onClick = {}
+                ),
+                onChange = notesViewModel::changeSearchText,
+            )
+            NoteListScreenActionBar(actionBarHeight, offsetX, notesViewModel)
+        }
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(35.dp)
+        ) {
+            AnimatedVisibility(
+                notesViewModel.screenState.value.selectedNotes.isEmpty(),
+                enter = fadeIn(tween(200, easing = LinearOutSlowInEasing)),
+                exit = fadeOut(tween(200, easing = FastOutSlowInEasing)),
+            ) {
+                FloatingActionButton(isActive = notesViewModel.screenState.value.selectedNotes.isEmpty()) {
+                    notesViewModel.createNewNote()
+                }
+            }
+        }
+    }
+
+    NoteListScreenReminderCheck(notesViewModel)
+    CreateReminderContent(notesViewModel)
+}
+
+
+@Composable
+private fun WhenUserCreateNewNote(
+    notesViewModel: NotesViewModel,
+    navController: NavController
+) {
+    /**
+     * LaunchedEffect when new note created.
+     */
+    LaunchedEffect(notesViewModel.screenState.value.lastCreatedNoteId) {
+        if (notesViewModel.screenState.value.lastCreatedNoteId.isNotEmpty()) {
+            navController.navigate(AppScreens.Note.noteId(notesViewModel.screenState.value.lastCreatedNoteId))
+            notesViewModel.clearLastCreatedNote()
+        }
+    }
+}
+
+@Composable
+private fun IfScrollingImposible(
+    lazyGridNotes: LazyStaggeredGridState,
+    searchBarOffsetHeightPx: MutableState<Float>
+) {
+    /**
+     * LaunchedEffect for cases when it is impossible to scroll the list.
+     */
+    LaunchedEffect(lazyGridNotes.canScrollForward, lazyGridNotes.canScrollBackward) {
+        if (!lazyGridNotes.canScrollForward && !lazyGridNotes.canScrollBackward) {
+            searchBarOffsetHeightPx.value = 0f
+        }
+    }
+}
+
+@Composable
+private fun IfSelectedNotesChanged(
+    notesViewModel: NotesViewModel,
+    offsetX: Animatable<Float, AnimationVector1D>,
+    actionBarHeightPx: Float
+) {
+    /**
+     * LaunchedEffect for cases when the number of selected notes changes.
+     */
+    LaunchedEffect(notesViewModel.screenState.value.selectedNotes) {
+        if (notesViewModel.screenState.value.selectedNotes.isEmpty()) {
+            offsetX.animateTo(
+                targetValue = -actionBarHeightPx, animationSpec = tween(durationMillis = 200)
+            )
+        } else {
+            offsetX.animateTo(
+                targetValue = 0f, animationSpec = tween(durationMillis = 200)
+            )
+        }
+    }
+}
+
+@Composable
+private fun BackHandlerIfSelectedNotes(notesViewModel: NotesViewModel) {
+    /**
+     * When user select note
+     */
+    BackHandler(
+        enabled = notesViewModel.screenState.value.selectedNotes.isNotEmpty(), onBack =
+        notesViewModel::clearSelectedNote
+    )
+}
+
+
+@Composable
+private fun NotesListContent(
+    notesViewModel: NotesViewModel,
+    lazyGridNotes: LazyStaggeredGridState,
+    navController: NavController,
+    currentRoute: String
+) {
+    val pinnedNotes = notesViewModel.screenState.value.notes.filter { it.isPinned }
+    val unpinnedNotes = notesViewModel.screenState.value.notes.filterNot { it.isPinned }
+
+    LazyGridNotes(
+        contentPadding = PaddingValues(10.dp, 70.dp, 10.dp, 80.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag("notes_list"),
+        gridState = lazyGridNotes
+    ) {
+        item(span = StaggeredGridItemSpan.FullLine) {
+            BannerList(navController)
+        }
+        if (notesViewModel.screenState.value.hashtags.isNotEmpty())
+            item(span = StaggeredGridItemSpan.FullLine) {
+                LazyRow(
+                    modifier = Modifier
+                        .wrapContentSize(unbounded = true)
+                        .width(LocalConfiguration.current.screenWidthDp.dp),
+                    state = rememberLazyListState(),
+                    contentPadding = PaddingValues(10.dp, 0.dp, 10.dp, 0.dp)
+                ) {
+                    items(items = notesViewModel.screenState.value.hashtags.toList()) { item ->
+                        HashtagButton(item, item == notesViewModel.screenState.value.currentHashtag) {
+                            notesViewModel.setCurrentHashtag(
+                                item
+                            )
+                        }
+                    }
+                }
+            }
+        if (pinnedNotes.isNotEmpty()) {
+            item(span = StaggeredGridItemSpan.FullLine) {
+                Column {
+                    Text(
+                        stringResource(R.string.Pinned),
+                        color = CustomTheme.colors.textSecondary,
+                        style = MaterialTheme.typography.body1.copy(fontSize = 17.sp),
+                        modifier = Modifier.padding(horizontal = 10.dp)
+                    )
+                }
+            }
+            items(items = pinnedNotes) { item ->
+                NoteCard(item.header,
+                    item.body,
+                    reminder = notesViewModel.screenState.value.reminders.find { it.noteId == item._id },
+                    markedText = notesViewModel.screenState.value.searchText,
+                    selected = item._id in notesViewModel.screenState.value.selectedNotes,
+                    onClick = { notesViewModel.clickOnNote(item, currentRoute, navController) },
+                    onLongClick = { notesViewModel.toggleSelectedNoteCard(item._id) })
+            }
+        }
+        if (unpinnedNotes.isNotEmpty()) {
+            item(span = StaggeredGridItemSpan.FullLine) {
+                Text(
+                    stringResource(R.string.Other),
+                    color = CustomTheme.colors.textSecondary,
+                    style = MaterialTheme.typography.body1.copy(fontSize = 17.sp),
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+            }
+            items(items = unpinnedNotes) { item ->
+                NoteCard(item.header,
+                    item.body,
+                    reminder = notesViewModel.screenState.value.reminders.find { it.noteId == item._id },
+                    markedText = notesViewModel.screenState.value.searchText,
+                    selected = item._id in notesViewModel.screenState.value.selectedNotes,
+                    onClick = { notesViewModel.clickOnNote(item, currentRoute, navController) },
+                    onLongClick = { notesViewModel.toggleSelectedNoteCard(item._id) })
+            }
+        }
+    }
+}
+
+@Composable
+private fun CreateReminderContent(notesViewModel: NotesViewModel) {
+    if (notesViewModel.screenState.value.isCreateReminderDialogShow) {
+        val reminder =
+            remember {
+                if (notesViewModel.screenState.value.selectedNotes.size == 1)
+                    notesViewModel.getReminder(notesViewModel.screenState.value.selectedNotes.first()) else null
+            }
+        val reminderDialogProperties = remember {
+            if (reminder != null) ReminderDialogProperties(reminder.date, reminder.time, reminder.repeat)
+            else ReminderDialogProperties()
+        }
+        CreateReminderDialog(
+            reminderDialogProperties = reminderDialogProperties,
+            onGoBack = notesViewModel::switchReminderDialogShow,
+            onDelete = if (reminder != null) notesViewModel::deleteSelectedReminder else null,
+            onSave = notesViewModel::createReminder,
+        )
+    }
+}
