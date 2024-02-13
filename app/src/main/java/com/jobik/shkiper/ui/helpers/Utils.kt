f962ed1aa147431e2f72ee3acc47b1270cdc413e
EFFemole <go.netgle@gmail.com>
2024-02-13 22:33:23 +0200

    update

--- app/src/main/java/com/jobik/shkiper/ui/helpers/Utils.kt
+++ app/src/main/java/com/jobik/shkiper/ui/helpers/Utils.kt
@@ -1,9 +1,13 @@
 package com.jobik.shkiper.ui.helpers
 
 import androidx.compose.runtime.*
+import com.jobik.shkiper.database.models.Reminder
+import com.jobik.shkiper.helpers.DateHelper
 import com.kizitonwose.calendar.compose.weekcalendar.WeekCalendarState
 import com.kizitonwose.calendar.core.Week
 import kotlinx.coroutines.flow.filter
+import org.mongodb.kbson.ObjectId
+import java.time.LocalDateTime
 import java.time.YearMonth
 import java.time.format.TextStyle
 import java.util.*
@@ -27,4 +31,19 @@ fun rememberFirstVisibleWeekAfterScroll(state: WeekCalendarState): Week {
  */
 fun YearMonth.displayText(): String {
     return "${this.month.getDisplayName(TextStyle.FULL, Locale.getDefault())} ${this.year}"
+}
+
+@Composable
+fun rememberNextReminder(
+    reminders: List<Reminder>,
+    noteId: ObjectId,
+    pointDate: LocalDateTime = LocalDateTime.now()
+): Reminder? {
+    val nextReminder = remember { mutableStateOf<Reminder?>(null) }
+
+    LaunchedEffect(reminders) {
+        val noteReminders = reminders.filter { it.noteId == noteId }
+        nextReminder.value = DateHelper.sortReminders(reminders = noteReminders, pointDate = pointDate).firstOrNull()
+    }
+    return nextReminder.value
 }
