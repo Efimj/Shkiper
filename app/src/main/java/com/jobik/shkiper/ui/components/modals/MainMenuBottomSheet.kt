61d1269174a99abceb3a5c6ca3589596121e736c
EFFemole <go.netgle@gmail.com>
2024-03-21 21:35:04 +0200

    new components

--- app/src/main/java/com/jobik/shkiper/ui/components/modals/MainMenuBottomSheet.kt
+++ app/src/main/java/com/jobik/shkiper/ui/components/modals/MainMenuBottomSheet.kt
@@ -1,13 +1,9 @@
 package com.jobik.shkiper.ui.components.modals
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.TweenSpec
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
@@ -16,9 +12,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -27,47 +22,31 @@ import com.jobik.shkiper.R
 import com.jobik.shkiper.database.models.NotePosition
 import com.jobik.shkiper.navigation.SetupAppScreenNavGraph
 import com.jobik.shkiper.navigation.AppScreens
-import com.jobik.shkiper.ui.components.buttons.MainMenuButton
-import com.jobik.shkiper.ui.components.buttons.CustomButton
-import com.jobik.shkiper.ui.components.buttons.DefaultButtonProperties
+import com.jobik.shkiper.navigation.NavigationHelpers.Companion.canNavigate
 import com.jobik.shkiper.ui.components.cards.SnackbarCard
+import com.jobik.shkiper.screens.AppLayout.CustomBottomNavigation
+import com.jobik.shkiper.screens.AppLayout.CustomBottomNavigationItem
 import com.jobik.shkiper.ui.theme.CustomTheme
 import com.jobik.shkiper.util.MainMenuButtonState
 import com.jobik.shkiper.util.SnackbarHostUtil
 import com.jobik.shkiper.util.SnackbarVisualsCustom
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Composable
-@OptIn(ExperimentalMaterialApi::class)
-fun MainMenuBottomSheet(startDestination: String = AppScreens.NoteList.route) {
-    val coroutineScope = rememberCoroutineScope()
+fun AppLayout(startDestination: String = AppScreens.NoteList.route) {
     val navController = rememberNavController()
-    val bottomSheetState =
-        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden, skipHalfExpanded = true)
 
-    ModalBottomSheetLayout(
-        sheetBackgroundColor = CustomTheme.colors.secondaryBackground,
-        sheetState = bottomSheetState,
-        scrimColor = CustomTheme.colors.modalBackground,
-        sheetShape = RoundedCornerShape(topStart = 26.dp, topEnd = 26.dp),
-        sheetContent = {
-            BottomSheetContent(navController, coroutineScope, bottomSheetState)
-        }
-    ) {
-        MainPageLayout(navController, startDestination, coroutineScope, bottomSheetState)
-    }
+    MainPageLayout(navController, startDestination)
 }
 
 @Composable
-@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalAnimationApi::class)
 private fun MainPageLayout(
     navController: NavHostController,
     startDestination: String = AppScreens.NoteList.route,
-    coroutineScope: CoroutineScope,
-    bottomSheetState: ModalBottomSheetState
 ) {
-    val menuContainerHeight = 40
+    val localDensity = LocalDensity.current
+    var navigationContainerHeight by remember { mutableStateOf(0.dp) }
+
     val currentRoute =
         (navController.currentBackStackEntryAsState().value?.destination?.route ?: "").substringBefore("/")
 
@@ -76,8 +55,8 @@ private fun MainPageLayout(
     }
 
     val offsetY by animateDpAsState(
-        if (MainMenuButtonState.isButtonOpened.value) (menuContainerHeight).dp else 0.dp,
-        animationSpec = TweenSpec(durationMillis = 200), label = "offsetY"
+        if (MainMenuButtonState.isButtonOpened.value) (navigationContainerHeight) else 0.dp,
+        animationSpec = spring(stiffness = Spring.StiffnessMedium), label = "offsetY"
     )
 
     Box(Modifier.fillMaxSize()) {
@@ -86,19 +65,6 @@ private fun MainPageLayout(
                 navController = navController,
                 startDestination = if (startDestination == AppScreens.Onboarding.route) AppScreens.Onboarding.route else AppScreens.NoteList.route
             )
-        }
-        SnackbarHost(
-            hostState = SnackbarHostUtil.snackbarHostState,
-            modifier = Modifier.align(Alignment.BottomCenter)
-        ) { snackbarData ->
-            Box(
-                Modifier
-                    .offset(y = (-30).dp)
-                    .align(Alignment.BottomCenter)
-            ) {
-                val customVisuals = snackbarData.visuals as SnackbarVisualsCustom
-                SnackbarCard(customVisuals)
-            }
         }
         Box(
             modifier = Modifier
@@ -113,34 +79,27 @@ private fun MainPageLayout(
                 ),
             contentAlignment = Alignment.BottomCenter
         ) {
-            CustomButton(
-                text = getCurrentMenuText(currentRoute),
-                icon = navController.currentBackStackEntryAsState().value?.destination?.route?.let {
-                    getCurrentMenuIcon(
-                        it
-                    )
-                },
-                onClick = {
-                    coroutineScope.launch {
-                        bottomSheetState.show()
-                    }
-                },
-                properties = DefaultButtonProperties(
-                    buttonColors = ButtonDefaults.buttonColors(
-                        backgroundColor = CustomTheme.colors.secondaryBackground,
-                        disabledBackgroundColor = Color.Transparent
-                    ),
-                    iconTint = CustomTheme.colors.text,
-                    textColor = CustomTheme.colors.text,
-                    shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
-                    horizontalPaddings = 20.dp,
-                    border = BorderStroke(width = 1.dp, color = CustomTheme.colors.stroke)
-                ),
+            Navigation(
                 modifier = Modifier
-                    .height(menuContainerHeight.dp)
-                    .width(160.dp)
-                    .offset(y = 1.dp),
+                    .onGloballyPositioned { coordinates ->
+                        // Set screen height using the LayoutCoordinates
+                        navigationContainerHeight = with(localDensity) { coordinates.size.height.toDp() }
+                    },
+                navController = navController
             )
+        }
+        SnackbarHost(
+            hostState = SnackbarHostUtil.snackbarHostState,
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) { snackbarData ->
+            Box(
+                Modifier
+                    .offset(y = -navigationContainerHeight)
+                    .align(Alignment.BottomCenter)
+            ) {
+                val customVisuals = snackbarData.visuals as SnackbarVisualsCustom
+                SnackbarCard(customVisuals)
+            }
         }
     }
     val isInitialized = rememberSaveable { mutableStateOf(false) }
@@ -154,122 +113,74 @@ private fun MainPageLayout(
     }
 }
 
-private fun getCurrentMenuIcon(currentRoute: String): ImageVector {
-    val currentButtonIcon = when (currentRoute.substringBefore("/")) {
-        AppScreens.NoteList.route.substringBefore("/") ->
-            Icons.Outlined.AutoAwesomeMosaic
-
-        AppScreens.Archive.route.substringBefore("/") ->
-            Icons.Outlined.Archive
-
-        AppScreens.Basket.route.substringBefore("/") ->
-            Icons.Outlined.Delete
-
-        AppScreens.Settings.route.substringBefore("/") ->
-            Icons.Outlined.Settings
-
-        else -> Icons.Outlined.Menu
-    }
-    return currentButtonIcon
-}
-
 @Composable
-private fun getCurrentMenuText(currentRoute: String): String {
-    val currentButtonIcon = when (currentRoute.substringBefore("/")) {
-        AppScreens.NoteList.route.substringBefore("/") ->
-            stringResource(R.string.Notes)
-
-        AppScreens.Archive.route.substringBefore("/") ->
-            stringResource(R.string.Archive)
-
-        AppScreens.Basket.route.substringBefore("/") ->
-            stringResource(R.string.Basket)
-
-        AppScreens.Settings.route.substringBefore("/") ->
-            stringResource(R.string.Settings)
-
-        else -> stringResource(R.string.Menu)
-    }
-    return currentButtonIcon
-}
-
-@Composable
-@OptIn(ExperimentalMaterialApi::class)
-private fun BottomSheetContent(
+private fun Navigation(
+    modifier: Modifier,
     navController: NavHostController,
-    coroutineScope: CoroutineScope,
-    bottomSheetState: ModalBottomSheetState
 ) {
     val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
-    Column(
-        modifier = Modifier
+    val navigationItems = listOf(
+        CustomBottomNavigationItem(
+            icon = Icons.Outlined.AutoAwesomeMosaic,
+            isSelected = currentRoute == AppScreens.NoteList.route,
+            description = R.string.Notes,
+        ) {
+            goToPage(
+                navController = navController,
+                rout = AppScreens.NoteList.notePosition(NotePosition.MAIN.name)
+            )
+        },
+        CustomBottomNavigationItem(
+            icon = Icons.Outlined.Archive,
+            isSelected = currentRoute == AppScreens.Archive.route,
+            description = R.string.Archive,
+        ) {
+            goToPage(
+                navController = navController,
+                rout = AppScreens.Archive.notePosition(NotePosition.ARCHIVE.name),
+            )
+        },
+        CustomBottomNavigationItem(
+            icon = Icons.Outlined.Delete,
+            isSelected = currentRoute == AppScreens.Basket.route,
+            description = R.string.Basket,
+        ) {
+            goToPage(
+                navController = navController,
+                rout = AppScreens.Basket.notePosition(NotePosition.DELETE.name),
+            )
+        },
+        CustomBottomNavigationItem(
+            icon = Icons.Outlined.Settings,
+            isSelected = currentRoute == AppScreens.Settings.route,
+            description = R.string.Settings
+        ) {
+            goToPage(
+                navController = navController,
+                rout = AppScreens.Settings.route,
+            )
+        },
+    )
+
+    Row(
+        modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(bottom = 20.dp), horizontalArrangement = Arrangement.Center
     ) {
-        MainMenuButton(stringResource(R.string.Notes),
-            Icons.Outlined.AutoAwesomeMosaic,
-            isActive = currentRoute == AppScreens.NoteList.route,
-            onClick = {
-                goToPage(
-                    navController,
-                    AppScreens.NoteList.notePosition(NotePosition.MAIN.name),
-                    coroutineScope,
-                    bottomSheetState
-                )
-            })
-        Spacer(modifier = Modifier.height(8.dp))
-        MainMenuButton(stringResource(R.string.Archive),
-            Icons.Outlined.Archive,
-            isActive = currentRoute == AppScreens.Archive.route,
-            onClick = {
-                goToPage(
-                    navController,
-                    AppScreens.Archive.notePosition(NotePosition.ARCHIVE.name),
-                    coroutineScope,
-                    bottomSheetState
-                )
-            })
-        Spacer(modifier = Modifier.height(8.dp))
-        MainMenuButton(stringResource(R.string.Basket),
-            Icons.Outlined.Delete,
-            isActive = currentRoute == AppScreens.Basket.route,
-            onClick = {
-                goToPage(
-                    navController,
-                    AppScreens.Basket.notePosition(NotePosition.DELETE.name),
-                    coroutineScope,
-                    bottomSheetState
-                )
-            })
-        Spacer(modifier = Modifier.height(8.dp))
-        MainMenuButton(stringResource(R.string.Settings),
-            Icons.Outlined.Settings,
-            isActive = currentRoute == AppScreens.Settings.route,
-            onClick = {
-                goToPage(
-                    navController,
-                    AppScreens.Settings.route,
-                    coroutineScope,
-                    bottomSheetState
-                )
-            }
-        )
+        CustomBottomNavigation(navigationItems)
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
 private fun goToPage(
     navController: NavHostController,
     rout: String,
-    coroutineScope: CoroutineScope,
-    modalBottomSheetState: ModalBottomSheetState
 ) {
-    if (navController.currentDestination?.route?.substringBefore("/") == rout.substringBefore("/")) {
-        coroutineScope.launch { modalBottomSheetState.hide() }
+    if (navController.canNavigate()
+            .not() || navController.currentDestination?.route?.substringBefore("/") == rout.substringBefore("/")
+    ) {
         return
     }
-    coroutineScope.launch { modalBottomSheetState.hide() }
     navController.navigate(rout) {
         launchSingleTop
     }
