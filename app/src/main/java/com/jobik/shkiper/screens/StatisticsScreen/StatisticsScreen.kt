e91908b60c304724b44a45a1a8b8d89f58cc33ed
EFFemole(notebook) <efim1tv@gmail.com>
2024-04-08 01:18:01 +0300

    updated material typography

--- app/src/main/java/com/jobik/shkiper/screens/StatisticsScreen/StatisticsScreen.kt
+++ app/src/main/java/com/jobik/shkiper/screens/StatisticsScreen/StatisticsScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.outlined.ContentCopy
