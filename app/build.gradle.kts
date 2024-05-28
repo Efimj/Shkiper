9a66dd8c44ec0dff45382bf8fd9ca4fac710b58f
EFFemole <go.netgle@gmail.com>
2024-05-28 20:49:50 +0300

    dependencies are put into a file libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,98 +92,94 @@ android {
     }
 }
 
-val composeVersion by extra("1.5.4")
-val accompanistVersion by extra("0.32.0")
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.compose.ui:ui:${project.extra["composeVersion"]}")
-    implementation("androidx.compose.ui:ui-tooling-preview:${project.extra["composeVersion"]}")
-    implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${project.extra["composeVersion"]}")
-    debugImplementation("androidx.compose.ui:ui-tooling:${project.extra["composeVersion"]}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${project.extra["composeVersion"]}")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 
     // Splash screen API (not work when android less than 12)
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation(libs.androidx.core.splashscreen)
 
     // Navigation Compose
     // implementation("androidx.navigation:navigation-compose:$accompanist_version")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.4")
+    implementation(libs.androidx.navigation.runtime.ktx)
 
     // Pager and Indicators - Accompanist
     // implementation("com.google.accompanist:accompanist-pager:$accompanist_version")
-    implementation("com.google.accompanist:accompanist-pager-indicators:${project.extra["accompanistVersion"]}")
+    implementation(libs.accompanist.pager.indicators)
 
     // DataStore Preferences
     // implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     // Dagger - Hilt
-//    kapt("com.google.dagger:hilt-android-compiler:${project.extra["hiltVersion"]}")
-//    kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.hilt)
     kapt(libs.dagger.hilt.compiler)
 
     // Icons
-    implementation("androidx.compose.material:material-icons-extended:${project.extra["composeVersion"]}")
+    implementation(libs.androidx.material.icons.extended)
 
     // UI controller
-    implementation("com.google.accompanist:accompanist-systemuicontroller:${project.extra["accompanistVersion"]}")
+    implementation(libs.accompanist.systemuicontroller)
 
     // Livedata
     // implementation("androidx.compose.runtime:runtime-livedata:$compose_version")
 
     // Mongo Realm
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("io.realm.kotlin:library-base:1.16.0")
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.library.base)
     // implementation("io.realm:realm-android-library:10.9.0")
 
     // Wheel DateTime picker
-    implementation("com.github.commandiron:WheelPickerCompose:1.1.11")
+    implementation(libs.wheelpickercompose)
 
     // Customize Calendar
-    implementation("com.kizitonwose.calendar:compose:2.5.0")
+    implementation(libs.compose)
 
     // JSON serialization
-    implementation("com.google.code.gson:gson:2.10")
+    implementation(libs.gson)
 
     // Coil for images
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation(libs.coil.compose)
 
     // Jsoup for link preview
-    implementation("org.jsoup:jsoup:1.14.3")
+    implementation(libs.jsoup)
 
     // TedPermission permission provider
-    implementation("io.github.ParkSangGwon:tedpermission-coroutine:3.3.0")
+    implementation(libs.tedpermission.coroutine)
 
     // In-app updates
-    implementation("com.google.android.play:app-update:2.1.0")
-    implementation("com.google.android.play:app-update-ktx:2.1.0")
+    implementation(libs.app.update)
+    implementation(libs.app.update.ktx)
 
     // In-app purchases
-    implementation("com.android.billingclient:billing-ktx:6.0.1")
+    implementation(libs.billing.ktx)
 
     // Glance Widgets
     // For AppWidgets support
-    implementation("androidx.glance:glance-appwidget:1.0.0")
+    implementation(libs.androidx.glance.appwidget)
+
     // For interop APIs with Material 3
-    implementation("androidx.glance:glance-material3:1.0.0")
+    implementation(libs.androidx.glance.material3)
 
     // Compose Rich Editor
-    implementation("com.mohamedrejeb.richeditor:richeditor-compose:1.0.0-rc05-k2")
+    implementation(libs.richeditor.compose)
 
     // Compose image capture
-    implementation("dev.shreyaspatil:capturable:1.0.3")
+    implementation(libs.capturable)
 
     // Compose collapsing toolbar (Official latest update in 2022)
     // implementation("me.onebone:toolbar-compose:2.3.5")
     // Compose collapsing toolbar (nonofficial)
-    implementation("com.github.GIGAMOLE:ComposeCollapsingToolbar:1.0.8")
+    implementation(libs.composecollapsingtoolbar)
 }
