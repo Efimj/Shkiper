8b58e4ba4bd18fe2727915382200ba7d555edaad
EFFemole <go.netgle@gmail.com>
2024-06-08 19:48:46 +0300

    feat: new LauncherIcon

---
+++ app/src/main/java/com/jobik/shkiper/util/LauncherIcon.kt
@@ -0,0 +1,40 @@
+package com.jobik.shkiper.util
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.annotation.DrawableRes
+import androidx.annotation.Keep
+import com.jobik.shkiper.R
+
+class LauncherIcon {
+    @Keep
+    enum class LauncherActivity(val activity: String, @DrawableRes val drawable: Int) {
+        Default(activity = "activity.SplashActivity", drawable = R.drawable.ic_app),
+        Minimalism(activity = "MinimalismActivity", drawable = R.drawable.ic_minimalism);
+
+        fun toActivityAliasName(context: Context): String {
+            println(context.packageName)
+            return this.activity.let { context.packageName + ".$it" }
+        }
+    }
+
+    fun switchLauncherIcon(context: Context, activity: LauncherActivity) {
+        val packageManager = context.packageManager
+        LauncherActivity.entries.forEach {
+            val state = if (it.activity == activity.activity)
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+            else
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+
+            packageManager.setComponentEnabledSetting(
+                /* componentName = */ ComponentName(
+                    context,
+                    it.toActivityAliasName(context = context)
+                ),
+                /* newState = */ state,
+                /* flags = */ PackageManager.DONT_KILL_APP
+            )
+        }
+    }
+}
