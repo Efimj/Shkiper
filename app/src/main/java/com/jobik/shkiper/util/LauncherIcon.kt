d74b19d5f503c6286a4fe4348a93149fef75fc07
EFFemole <go.netgle@gmail.com>
2024-06-09 17:09:45 +0300

    feat: launcher icons updated

--- app/src/main/java/com/jobik/shkiper/util/LauncherIcon.kt
+++ app/src/main/java/com/jobik/shkiper/util/LauncherIcon.kt
@@ -10,11 +10,37 @@ import com.jobik.shkiper.R
 class LauncherIcon {
     @Keep
     enum class LauncherActivity(val activity: String, @DrawableRes val drawable: Int) {
-        Default(activity = "activity.MainActivity", drawable = R.drawable.ic_app),
-        Minimalism(activity = "MaterialActivity", drawable = R.drawable.ic_notification);
+        Classic(
+            activity = "activity.MainActivity",
+            drawable = R.drawable.ic_classic_launcher_foreground
+        ),
+        ClassicWhite(
+            activity = "ClassicWhiteLauncher",
+            drawable = R.drawable.ic_classic_white_launcher_foreground
+        ),
+        Avocado(activity = "AvocadoLauncher", drawable = R.drawable.ic_avocado_launcher_foreground),
+        Blueberry(
+            activity = "BlueberryLauncher",
+            drawable = R.drawable.ic_blueberry_launcher_foreground
+        ),
+        Mango(
+            activity = "MangoLauncher",
+            drawable = R.drawable.ic_mango_launcher_foreground
+        ),
+        Plum(
+            activity = "PlumLauncher",
+            drawable = R.drawable.ic_plum_launcher_foreground
+        ),
+        Strawberry(
+            activity = "StrawberryLauncher",
+            drawable = R.drawable.ic_strawberry_launcher_foreground
+        ),
+        Material(
+            activity = "MaterialLauncher",
+            drawable = R.drawable.ic_material_launcher_foreground
+        );
 
         fun toActivityAliasName(context: Context): String {
-            println(context.packageName)
             return this.activity.let { context.packageName + ".$it" }
         }
     }
