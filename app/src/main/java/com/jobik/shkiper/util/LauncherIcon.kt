7b3924a1b3e78cb2e7633f3a3bc53758adebe242
EFFemole <go.netgle@gmail.com>
2024-06-09 19:14:52 +0300

    feat: added launcher to preview in app

--- app/src/main/java/com/jobik/shkiper/util/LauncherIcon.kt
+++ app/src/main/java/com/jobik/shkiper/util/LauncherIcon.kt
@@ -12,32 +12,32 @@ class LauncherIcon {
     enum class LauncherActivity(val activity: String, @DrawableRes val drawable: Int) {
         Classic(
             activity = "activity.MainActivity",
-            drawable = R.drawable.ic_classic_launcher_foreground
+            drawable = R.drawable.ic_classic_launcher
         ),
         ClassicWhite(
             activity = "ClassicWhiteLauncher",
-            drawable = R.drawable.ic_classic_white_launcher_foreground
+            drawable = R.drawable.ic_classic_white_launcher
         ),
-        Avocado(activity = "AvocadoLauncher", drawable = R.drawable.ic_avocado_launcher_foreground),
+        Avocado(activity = "AvocadoLauncher", drawable = R.drawable.ic_avocado_launcher),
         Blueberry(
             activity = "BlueberryLauncher",
-            drawable = R.drawable.ic_blueberry_launcher_foreground
+            drawable = R.drawable.ic_blueberry_launcher
         ),
         Mango(
             activity = "MangoLauncher",
-            drawable = R.drawable.ic_mango_launcher_foreground
+            drawable = R.drawable.ic_mango_launcher
         ),
         Plum(
             activity = "PlumLauncher",
-            drawable = R.drawable.ic_plum_launcher_foreground
+            drawable = R.drawable.ic_plum_launcher
         ),
         Strawberry(
             activity = "StrawberryLauncher",
-            drawable = R.drawable.ic_strawberry_launcher_foreground
+            drawable = R.drawable.ic_strawberry_launcher
         ),
         Material(
             activity = "MaterialLauncher",
-            drawable = R.drawable.ic_material_launcher_foreground
+            drawable = R.drawable.ic_material_launcher
         );
 
         fun toActivityAliasName(context: Context): String {
