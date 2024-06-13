d418a7f281399efa08f3e5400065cfb99c2f4d92
EFFemole <go.netgle@gmail.com>
2024-06-13 16:54:58 +0300

    feat: updated icon

--- app/src/main/java/com/jobik/shkiper/screens/layout/NavigationBar/BottomAppBarProvider.kt
+++ app/src/main/java/com/jobik/shkiper/screens/layout/NavigationBar/BottomAppBarProvider.kt
@@ -4,7 +4,7 @@ import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.MaterialTheme
