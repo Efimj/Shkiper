d865efab8c213d32439a65af1326134afe71e5ef
EFFemole <go.netgle@gmail.com>
2024-06-14 13:58:27 +0300

    feat: added context extension fun isInstalledFromPlayStore

--- app/src/main/java/com/jobik/shkiper/util/ContextUtils.kt
+++ app/src/main/java/com/jobik/shkiper/util/ContextUtils.kt
@@ -1,6 +1,7 @@
 package com.jobik.shkiper.util
 
 import android.content.Context
+import android.os.Build
 
 object ContextUtils {
     fun Context.adjustFontSize(
@@ -9,5 +10,26 @@ object ContextUtils {
         val configuration = resources.configuration
         configuration.fontScale = scale ?: resources.configuration.fontScale
         return createConfigurationContext(configuration)
+    }
+
+    fun Context.isInstalledFromPlayStore(): Boolean = verifyInstallerId(
+        listOf(
+            "com.android.vending",
+            "com.google.android.feedback"
+        )
+    )
+
+    private fun Context.verifyInstallerId(
+        validInstallers: List<String>
+    ): Boolean = validInstallers.contains(getInstallerPackageName(packageName))
+
+    private fun Context.getInstallerPackageName(packageName: String): String? {
+        kotlin.runCatching {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                return packageManager.getInstallSourceInfo(packageName).installingPackageName
+            @Suppress("DEPRECATION")
+            return packageManager.getInstallerPackageName(packageName)
+        }
+        return null
     }
 }
