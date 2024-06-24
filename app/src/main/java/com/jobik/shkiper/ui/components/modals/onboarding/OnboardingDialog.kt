9a03a92f838f6a0eab1cec39b4107ed9e66a0d52
EFFemole <go.netgle@gmail.com>
2024-06-24 11:19:43 +0300

    feat: onboarding BottomNavigation

--- app/src/main/java/com/jobik/shkiper/ui/components/modals/onboarding/OnboardingDialog.kt
+++ app/src/main/java/com/jobik/shkiper/ui/components/modals/onboarding/OnboardingDialog.kt
@@ -2,10 +2,15 @@ package com.jobik.shkiper.ui.components.modals.onboarding
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
@@ -13,24 +18,35 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.jobik.shkiper.R
 import com.jobik.shkiper.ui.components.layouts.VerticalIndicator
 import com.jobik.shkiper.ui.components.modals.FullscreenPopup
-import com.jobik.shkiper.ui.helpers.startWindowInsetsPadding
+import com.jobik.shkiper.ui.helpers.bottomWindowInsetsPadding
+import com.jobik.shkiper.ui.helpers.horizontalWindowInsetsPadding
 import com.jobik.shkiper.ui.helpers.verticalWindowInsetsPadding
 import com.jobik.shkiper.ui.theme.AppTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun OnboardingDialog(isVisible: Boolean, onFinish: () -> Unit) {
@@ -67,6 +83,7 @@ fun OnboardingDialog(isVisible: Boolean, onFinish: () -> Unit) {
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
+                        .horizontalWindowInsetsPadding()
                         .background(AppTheme.colors.background)
                 ) {
                     IndicatorContent(pagerState = pagerState)
@@ -81,7 +98,7 @@ fun OnboardingDialog(isVisible: Boolean, onFinish: () -> Unit) {
                     ) {
 //                        OnBoardingScreen()
                     }
-                    BottomNavigation(pagerState = pagerState, onFinish)
+                    BottomNavigation(pagerState = pagerState, onFinish = onFinish)
                 }
             }
         }
@@ -90,8 +107,18 @@ fun OnboardingDialog(isVisible: Boolean, onFinish: () -> Unit) {
 
 @Composable
 private fun BoxScope.BottomNavigation(pagerState: PagerState, onFinish: () -> Unit) {
+    val scope = rememberCoroutineScope()
+    val isFinish = pagerState.currentPage >= pagerState.pageCount - 1
+
+    val (color, background) = listOf(
+        animateColorAsState(targetValue = if (isFinish) AppTheme.colors.onPrimary else AppTheme.colors.onSecondaryContainer).value,
+        animateColorAsState(targetValue = if (isFinish) AppTheme.colors.primary else AppTheme.colors.secondaryContainer).value
+    )
+
     Box(
-        modifier = Modifier.align(Alignment.BottomCenter),
+        modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .bottomWindowInsetsPadding(),
         contentAlignment = Alignment.BottomCenter
     ) {
         Row(
@@ -101,7 +128,51 @@ private fun BoxScope.BottomNavigation(pagerState: PagerState, onFinish: () -> Un
                 .padding(bottom = 20.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-
+            Button(modifier = Modifier
+                .weight(1f)
+                .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = color,
+                    containerColor = background
+                ),
+                onClick = {
+                    if (isFinish) {
+                        onFinish()
+                    } else {
+                        scope.launch {
+                            pagerState.animateScrollToPage(
+                                page = pagerState.currentPage + 1,
+                                animationSpec = spring(
+                                    dampingRatio = Spring.DampingRatioNoBouncy,
+                                    stiffness = Spring.StiffnessLow
+                                )
+                            )
+                        }
+                    }
+                }) {
+                Crossfade(
+                    targetState = isFinish,
+                    label = "FinishButton"
+                ) {
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        if (it) {
+                            Text(
+                                modifier = Modifier.basicMarquee(),
+                                text = stringResource(id = R.string.Finish),
+                                maxLines = 1,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        } else {
+                            Text(
+                                modifier = Modifier.basicMarquee(),
+                                text = stringResource(id = R.string.Next),
+                                maxLines = 1,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -111,7 +182,6 @@ private fun BoxScope.IndicatorContent(pagerState: PagerState) {
     Box(
         modifier = Modifier
             .verticalWindowInsetsPadding()
-            .startWindowInsetsPadding()
             .align(Alignment.TopStart),
         contentAlignment = Alignment.CenterStart
     ) {
