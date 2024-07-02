1ab661198aee00c8971bdef3309a684e12c9bd03
EFFemole <go.netgle@gmail.com>
2024-07-02 20:22:07 +0300

    feat: updated purchase screen ui

--- app/src/main/java/com/jobik/shkiper/screens/purchase/PurchaseScreen.kt
+++ app/src/main/java/com/jobik/shkiper/screens/purchase/PurchaseScreen.kt
@@ -1,12 +1,27 @@
 package com.jobik.shkiper.screens.purchase
 
 import android.app.Activity
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,13 +30,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.SignalWifiOff
+import androidx.compose.material.icons.outlined.VolunteerActivism
 import androidx.compose.material.icons.rounded.Verified
+import androidx.compose.material.icons.rounded.VolunteerActivism
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -31,10 +51,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -46,13 +68,20 @@ import com.android.billingclient.api.ProductDetails
 import com.jobik.shkiper.R
 import com.jobik.shkiper.services.billing.AppProducts
 import com.jobik.shkiper.ui.components.layouts.ScreenStub
-import com.jobik.shkiper.ui.components.modals.ImageActionDialog
-import com.jobik.shkiper.ui.components.modals.ImageActionDialogButton
+import com.jobik.shkiper.ui.components.modals.FullscreenPopup
 import com.jobik.shkiper.ui.helpers.allWindowInsetsPadding
 import com.jobik.shkiper.ui.modifiers.bounceClick
 import com.jobik.shkiper.ui.theme.AppTheme
 import com.jobik.shkiper.util.ContextUtils
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import nl.dionsegijn.konfetti.compose.KonfettiView
+import nl.dionsegijn.konfetti.core.Angle
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.Spread
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 @Composable
@@ -66,23 +95,7 @@ fun PurchaseScreen(purchaseViewModel: PurchaseViewModel = hiltViewModel()) {
         )
     }
 
-    if (purchaseViewModel.screenState.value.showGratitude) {
-        val stringResources = listOf(
-            R.string.ThankForPurchase1,
-            R.string.ThankForPurchase2,
-            R.string.ThankForPurchase3
-        )
-        ImageActionDialog(
-            header = stringResource(stringResources[Random.nextInt(stringResources.size)]),
-            onGoBack = {},
-            confirmButton = ImageActionDialogButton(
-                icon = Icons.Default.Favorite,
-                isActive = true,
-                onClick = purchaseViewModel::hideCompletedPurchase
-            )
-        )
-    }
-
+    Congratulations(purchaseViewModel)
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -95,6 +108,147 @@ fun PurchaseScreen(purchaseViewModel: PurchaseViewModel = hiltViewModel()) {
             )
         } else {
             ScreenContent(purchaseViewModel = purchaseViewModel)
+        }
+    }
+}
+
+@Composable
+private fun Congratulations(purchaseViewModel: PurchaseViewModel) {
+    val titleRes by rememberSaveable {
+        mutableIntStateOf(
+            listOf(
+                R.string.ThankForPurchase1,
+                R.string.ThankForPurchase2,
+                R.string.ThankForPurchase3
+            ).random()
+        )
+    }
+
+    fun parade(): List<Party> {
+        val party = Party(
+            speed = 10f,
+            maxSpeed = 30f,
+            damping = 0.9f,
+            angle = Angle.RIGHT - 45,
+            spread = Spread.SMALL,
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(30),
+            position = Position.Relative(0.0, 0.35)
+        )
+
+        return listOf(
+            party,
+            party.copy(
+                angle = party.angle - 90, // flip angle from right to left
+                position = Position.Relative(1.0, 0.35)
+            ),
+        )
+    }
+
+    val animationState = remember { MutableTransitionState(false) }
+    val visible = remember { mutableStateOf(false) }
+
+    LaunchedEffect(purchaseViewModel.screenState.value.showGratitude) {
+        if (visible.value.not()) {
+            visible.value = true
+        }
+        animationState.targetState = purchaseViewModel.screenState.value.showGratitude
+    }
+
+    LaunchedEffect(animationState.isIdle) {
+        if (animationState.isIdle) {
+            visible.value = purchaseViewModel.screenState.value.showGratitude
+        }
+    }
+
+    if (visible.value) {
+        FullscreenPopup(
+            onDismiss = { },
+        ) {
+            AnimatedVisibility(
+                visibleState = animationState,
+                enter = fadeIn() + scaleIn(initialScale = .95f),
+                exit = fadeOut() + scaleOut(targetScale = .95f)
+            ) {
+                BackHandler(true) {}
+                var buttonHideShow by remember { mutableStateOf(false) }
+
+                LaunchedEffect(Unit) {
+                    delay(1500)
+                    buttonHideShow = true
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.3f))
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null,
+                            onClick = {}
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .widthIn(max = 450.dp)
+                            .fillMaxWidth()
+                            .clip(AppTheme.shapes.large)
+                            .background(AppTheme.colors.secondaryContainer)
+                            .padding(20.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.padding(bottom = 20.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(20.dp)
+                        ) {
+                            Box(Modifier.padding(horizontal = 10.dp)) {
+                                Icon(
+                                    modifier = Modifier.size(70.dp),
+                                    imageVector = Icons.Outlined.VolunteerActivism,
+                                    contentDescription = null,
+                                    tint = AppTheme.colors.primary
+                                )
+                            }
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = stringResource(titleRes),
+                                color = AppTheme.colors.onSecondaryContainer,
+                                fontWeight = FontWeight.SemiBold,
+                                overflow = TextOverflow.Ellipsis,
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        }
+                        androidx.compose.animation.AnimatedVisibility(
+                            visible = buttonHideShow,
+                            enter = slideInVertically { it } + expandVertically { -it },
+                            exit = slideOutVertically { it } + shrinkVertically { -it })
+                        {
+                            Button(modifier = Modifier
+                                .fillMaxWidth()
+                                .height(50.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    contentColor = AppTheme.colors.onPrimary,
+                                    containerColor = AppTheme.colors.primary
+                                ),
+                                onClick = { purchaseViewModel.hideCompletedPurchase() }) {
+                                Text(
+                                    modifier = Modifier.basicMarquee(),
+                                    text = stringResource(R.string.thank_you),
+                                    maxLines = 1,
+                                    style = MaterialTheme.typography.titleMedium,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            }
+                        }
+                    }
+                    KonfettiView(
+                        modifier = Modifier.fillMaxSize(),
+                        parties = parade(),
+                    )
+                }
+            }
         }
     }
 }
@@ -355,20 +509,6 @@ private fun SubscriptionCard(
                     maxLines = 1
                 )
             }
-//            if (isBought) {
-//                Box(
-//                    modifier = Modifier
-//                        .clip(CircleShape)
-//                        .background(AppTheme.colors.primary)
-//                        .padding(4.dp)
-//                ) {
-//                    Icon(
-//                        imageVector = Icons.Rounded.Verified,
-//                        contentDescription = null,
-//                        tint = AppTheme.colors.onPrimary
-//                    )
-//                }
-//            }
         }
         Row(
             modifier = Modifier
