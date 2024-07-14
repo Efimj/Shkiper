5600020f63b14c6a9517c6496cdcdc0c96ebc576
EFFemole <go.netgle@gmail.com>
2024-07-14 13:19:59 +0300

    feat: updated defaults for rich text styles, updated capturable note dialog

--- app/src/main/java/com/jobik/shkiper/ui/components/modals/ShareNoteDialog.kt
+++ app/src/main/java/com/jobik/shkiper/ui/components/modals/ShareNoteDialog.kt
@@ -4,38 +4,53 @@ import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.jobik.shkiper.R
 import com.jobik.shkiper.helpers.IntentHelper
+import com.jobik.shkiper.ui.helpers.SetRichTextDefaultStyles
 import com.jobik.shkiper.ui.theme.AppTheme
 import com.mohamedrejeb.richeditor.model.rememberRichTextState
 import com.mohamedrejeb.richeditor.ui.material3.RichText
-import dev.shreyaspatil.capturable.Capturable
+import dev.shreyaspatil.capturable.capturable
 import dev.shreyaspatil.capturable.controller.rememberCaptureController
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalComposeApi::class)
 @Composable
 fun ShareNoteDialog(
     noteHeader: String,
@@ -43,6 +58,7 @@ fun ShareNoteDialog(
     onConfirm: () -> Unit,
     onGoBack: () -> Unit,
 ) {
+    val scope = rememberCoroutineScope()
     val richTextState = rememberRichTextState()
     val captureController = rememberCaptureController()
     val context = LocalContext.current
@@ -52,83 +68,60 @@ fun ShareNoteDialog(
         label = "actionsBackgroundColor"
     )
 
-    val codeColor = AppTheme.colors.onPrimary
-    val codeBackgroundColor = AppTheme.colors.primary.copy(alpha = .2f)
-    val codeStrokeColor = AppTheme.colors.primary
-    val linkColor = AppTheme.colors.text
+    SetRichTextDefaultStyles(richTextState)
 
     LaunchedEffect(Unit) {
-        richTextState.setConfig(
-            linkColor = linkColor,
-            linkTextDecoration = TextDecoration.Underline,
-            codeColor = codeColor,
-            codeBackgroundColor = codeBackgroundColor,
-            codeStrokeColor = codeStrokeColor
-        )
-
         if (richTextState.annotatedString.text !== noteContent)
             richTextState.setHtml(noteContent)
         else
             richTextState.setText("")
     }
 
-    Dialog(onGoBack, DialogProperties(true, dismissOnClickOutside = true, usePlatformDefaultWidth = false)) {
+    Dialog(
+        onDismissRequest = onGoBack,
+        properties = DialogProperties(
+            true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false
+        )
+    ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(bottom = 10.dp)
                 .clip(AppTheme.shapes.medium)
-                .background(AppTheme.colors.background)
+                .background(AppTheme.colors.background),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Column(
                 modifier = Modifier
                     .weight(1f, fill = false)
                     .verticalScroll(noteContentScrollState)
             ) {
-                Capturable(
-                    controller = captureController,
-                    onCaptured = { bitmap, error ->
-                        if (bitmap != null) {
-                            IntentHelper().shareImageToOthers(
-                                context = context,
-                                text = "",
-                                bitmap = bitmap,
-                                format = Bitmap.CompressFormat.PNG
-                            )
-                            // handle success action share
-                            onConfirm()
-                        }
-
-                        if (error != null) {
-                            Log.d("Capturable error", error.toString())
-                            // Error occurred. Handle it!
-                        }
-                    }
+                Column(
+                    modifier = Modifier
+                        .capturable(captureController)
+                        .fillMaxWidth()
+                        .background(AppTheme.colors.background)
+                        .padding(20.dp)
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(AppTheme.colors.background)
-                            .padding(20.dp)
-                    ) {
-                        if (noteHeader.isNotEmpty()) {
-                            Text(
-                                text = noteHeader,
-                                style = MaterialTheme.typography.headlineSmall.copy(
-                                    fontWeight = FontWeight.SemiBold,
-                                ),
-                                color = AppTheme.colors.text
-                            )
-                        }
-                        if (richTextState.annotatedString.text.isNotEmpty() && noteHeader.isNotEmpty())
-                            Spacer(modifier = Modifier.height(4.dp))
-                        if (richTextState.annotatedString.text.isNotEmpty()) {
-                            RichText(
-                                state = richTextState,
-                                style = MaterialTheme.typography.bodyMedium,
-                                color = AppTheme.colors.text,
-                            )
-                        }
+                    if (noteHeader.isNotEmpty()) {
+                        Text(
+                            text = noteHeader,
+                            style = MaterialTheme.typography.headlineSmall.copy(
+                                fontWeight = FontWeight.SemiBold,
+                            ),
+                            color = AppTheme.colors.text
+                        )
+                    }
+                    if (richTextState.annotatedString.text.isNotEmpty() && noteHeader.isNotEmpty())
+                        Spacer(modifier = Modifier.height(4.dp))
+                    if (richTextState.annotatedString.text.isNotEmpty()) {
+                        RichText(
+                            state = richTextState,
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = AppTheme.colors.text,
+                        )
                     }
                 }
             }
@@ -172,7 +165,24 @@ fun ShareNoteDialog(
                     border = null,
                     elevation = null,
                     contentPadding = PaddingValues(horizontal = 15.dp),
-                    onClick = { captureController.capture() }
+                    onClick = {
+                        scope.launch {
+                            val capture = captureController.captureAsync()
+                            try {
+                                val bitmap = capture.await()
+                                IntentHelper().shareImageToOthers(
+                                    context = context,
+                                    text = "",
+                                    bitmap = bitmap,
+                                    format = Bitmap.CompressFormat.PNG
+                                )
+                                onConfirm()
+                            } catch (error: Throwable) {
+                                Log.d("Capturable error", error.toString())
+                                // Error occurred. Handle it!
+                            }
+                        }
+                    }
                 ) {
                     Icon(
                         imageVector = Icons.Outlined.Share,
